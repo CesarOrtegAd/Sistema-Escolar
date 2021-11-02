6da9bca97d382a42b5ec6645783df1ee68508f9f
Cesar <c.ortega2108198@gmail.com>
2021-11-01 18:04:26 -0600

    primera pantalla del software

---
+++ src/sistemaescolar/SistemaEscolar.java
@@ -0,0 +1,19 @@
+package sistemaescolar;
+
+import sistemaescolar.sistemaGUI.inicio;
+
+/**
+ *
+ * @author 80023
+ */
+public class SistemaEscolar {
+    
+    public static void main(String[] args) {
+        inicio login = new inicio();
+        
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+        
+    }
+    
+}
